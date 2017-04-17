d8f4b5c3b24d0325fc9c24cb6eebb032cd02bcfa
Jhon <jhonstupak@gmail.com>
2017-04-17 19:22:55 +0300

    Start

--- Demonstration/Demonstration.cs
+++ Demonstration/Demonstration.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demonstration
+namespace CandyGift
 {
-    public class Class1
+    public class Demonstration
     {
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
