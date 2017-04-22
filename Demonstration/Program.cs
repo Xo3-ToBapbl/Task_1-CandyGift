3c83f03c0e9bba1571a5fc88a72285a453d91aff
Jhon <jhonstupak@gmail.com>
2017-04-22 20:52:25 +0300

    Gift0.1

--- Demonstration/Program.cs
+++ Demonstration/Program.cs
@@ -5,19 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using CandyGift.SweetCandies;
 using CandyGift.SaltedCandies;
+using CandyGift.BaseCandyClasses;
 
 namespace Demonstration
 {
     class Program
     {
+        static void LINQ(Dictionary<int, Candy> Dict)
+        {
+            var container = from value in Dict.Values
+                            from key in Dict.Keys
+                            select key * value.Mass;
+
+
+            foreach (var item in container)
+                Console.WriteLine(item);
+        }
+
         static void Main(string[] args)
         {
-            Waffles HollandWaffles = new Waffles("Holland Waffles", 24, 5, "Jam");
-            HollandWaffles.GetProperties();
+            #region Data
+            Waffles HollandWaffles = new Waffles("Holland Waffles", 2, 5, "Jam");
+            LolliPop Lolli = new LolliPop("Lolli", 1, 11, "Cherry");
+            Cracker Cracen = new Cracker("Cracen", 1, 8);
 
-            Cracker Cracen = new Cracker("Cracen", 50, 8);
-            Cracen.GetProperties();
-
+            Dictionary<int, Candy> CandiesDict = new Dictionary<int, Candy>()
+            {
+                {5, HollandWaffles },
+                {6, Lolli },
+                {7, Cracen }
+            };
+            #endregion
+            LINQ(CandiesDict);
             Console.ReadKey();
         }
     }
