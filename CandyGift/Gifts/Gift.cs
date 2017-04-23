c1b888c3cca59f947bdefe6de927c23b8fba0bf1
Jhon <jhonstupak@gmail.com>
2017-04-23 14:21:26 +0300

    Gift0.2

---
+++ CandyGift/Gifts/Gift.cs
@@ -0,0 +1,45 @@
+﻿using CandyGift.BaseCandyClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyGift.Gifts
+{
+    public class Gift
+    {
+        private IDictionary<int, Candy> _candies;
+        private int _mass;
+
+        public string Name { get; }
+        public int Mass
+        {
+            get
+            {
+                return _mass;
+            }
+        }
+        public IDictionary<int, Candy> Candies
+        {
+            get
+            {
+                return new Dictionary<int, Candy>(_candies);
+            }
+        }
+
+        public Gift(string name, IDictionary<int, Candy> candies)
+        {
+            Name = name;
+            _candies = candies;
+            _mass = candies.Select(pair => pair.Key * pair.Value.Mass).Sum();
+        }
+
+        public void FindBySugar(int min, int max)
+        {
+            var sugar_list = from candy in _candies.Values
+                             where candy is SweetCandy
+                             select candy;
+        }
+    }
+}
--- Demonstration/Program.cs
+++ Demonstration/Program.cs
@@ -6,37 +6,33 @@ using System.Threading.Tasks;
 using CandyGift.SweetCandies;
 using CandyGift.SaltedCandies;
 using CandyGift.BaseCandyClasses;
+using CandyGift.Gifts;
 
 namespace Demonstration
 {
     class Program
     {
-        static void LINQ(Dictionary<int, Candy> Dict)
-        {
-            var container = from value in Dict.Values
-                            from key in Dict.Keys
-                            select key * value.Mass;
-
-
-            foreach (var item in container)
-                Console.WriteLine(item);
-        }
-
         static void Main(string[] args)
         {
             #region Data
-            Waffles HollandWaffles = new Waffles("Holland Waffles", 2, 5, "Jam");
-            LolliPop Lolli = new LolliPop("Lolli", 1, 11, "Cherry");
-            Cracker Cracen = new Cracker("Cracen", 1, 8);
+            Waffles HollandWaffles = new Waffles("Holland Waffles", 50, 25, "Jam");
+            ChocolateCandy Alenka = new ChocolateCandy("Alenka", 35, 18, "Jam");
+            LolliPop Lolli = new LolliPop("Lolli", 5, 11, "Cherry");
 
-            Dictionary<int, Candy> CandiesDict = new Dictionary<int, Candy>()
+            Cracker Cracen = new Cracker("Cracen", 10, 8);
+            Salmiak Lakrica = new Salmiak("Lakrica", 15, 12);
+
+            Dictionary<int, Candy> Candies = new Dictionary<int, Candy>()
             {
                 {5, HollandWaffles },
+                {3, Alenka },
                 {6, Lolli },
-                {7, Cracen }
+
+                {7, Cracen },
+                {5, Lakrica}
             };
             #endregion
-            LINQ(CandiesDict);
+            Gift MyGift = new Gift("MyGift", Candies);
             Console.ReadKey();
         }
     }
